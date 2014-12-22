ed1b262434a10147cdc42e57c4b1944a19546325
Erik Nelson <erik@nelsk.com>
2014-12-22 07:59:33 -0500

    Add brew list

--- ignore.sh
+++ ignore.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 ignoreFiles=(
-	"iterm2"
-	"README.md"
-	"scripts"
-	"oh-my-zsh"
-	"linux_screen_layout.sh"
-	"tmux.conf"
-	"openbox"
+  "iterm2"
+  "README.md"
+  "scripts"
+  "oh-my-zsh"
+  "linux_screen_layout.sh"
+  "tmux.conf"
+  "openbox"
+  "brew.list"
 )
