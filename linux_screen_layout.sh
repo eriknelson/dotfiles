c2c350a345b37ad7b4ca65ee60b70a4b9efe4583
Erik Nelson <erik@nelsk.com>
2015-01-21 08:47:14 -0500

    Triple monitor screen layout

--- linux_screen_layout.sh
+++ linux_screen_layout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --pos 1920x0 --rotate normal --output DVI-I-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-I-0 --off --output DP-1 --off --output DP-0 --off
+xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --pos 1920x1200 --rotate normal --output DVI-I-1 --mode 1920x1080 --pos 0x1200 --rotate normal --output DVI-I-0 --off --output DP-1 --off --output DP-0 --mode 1920x1200 --pos 144x0 --rotate normal
