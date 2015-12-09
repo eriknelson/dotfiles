83729ab5bc724ad5fbd759f3ae56f971b55b9b28
Erik Nelson <io@eriknelson.me>
2015-12-09 10:08:23 -0500

    Add easy ubuntu dep install flag

--- pyenv_install.sh
+++ pyenv_install.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+if [[ $* == *--install-ubuntu-dep* ]]; then
+  sudo apt-get install \
+    build-essential \
+    libssl-dev \
+    zlib1g-dev \
+    libsqlite3-dev \
+    libreadline-dev \
+    libbz2-dev
+  exit 1
+fi
+
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv
 git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
