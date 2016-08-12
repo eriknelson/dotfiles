0a36fba9c833705744d2f2d0f66c311b6580c1a3
Erik Nelson <erik@nsk.io>
2016-08-12 15:14:40 -0400

    Backup old vim settings and ignore

--- ignore.sh
+++ ignore.sh
@@ -1,44 +1,16 @@
 #!/bin/bash
-
-if [[ "$(uname)" == "Darwin" ]]; then
-  echo "Running Darwin"
-
-  ignoreFiles=(
-    "gulp-autocompletion-zsh"
-    "iterm2"
-    "README.md"
-    "scripts"
-    "oh-my-zsh"
-    "linux_screen_layout.sh"
-    "openbox"
-    "brew.list"
-    "base16-shell"
-    "Xresources"
-    "tmux.conf.linux"
-    "ref"
-    "pyenv_install.sh"
-    "i3"
-    "launch"
-    "nvim"
-  )
-elif [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
-  echo "Running Linux"
-
-  ignoreFiles=(
-    "gulp-autocompletion-zsh"
-    "iterm2"
-    "README.md"
-    "scripts"
-    "oh-my-zsh"
-    "linux_screen_layout.sh"
-    "openbox"
-    "brew.list"
-    "base16-shell"
-    "Xresources"
-    "tmux.conf.darwin"
-    "ref"
-    "pyenv_install.sh"
-    "launch"
-    "nvim"
-  )
-fi
+ignoreFiles=(
+  "gulp-autocompletion-zsh"
+  "iterm2"
+  "README.md"
+  "scripts"
+  "oh-my-zsh"
+  "linux_screen_layout.sh"
+  "openbox"
+  "base16-shell"
+  "Xresources"
+  "tmux.conf.darwin"
+  "pyenv_install.sh"
+  "launch"
+  "vim.b"
+)
