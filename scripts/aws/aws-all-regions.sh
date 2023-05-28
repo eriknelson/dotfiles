f19849e3329b1e3b00828a9f1406bd7696e68cc7
Erik Nelson <erik@nsk.io>
2023-05-28 08:09:47 -0400

    Add aws all regions script

--- scripts/aws/aws-all-regions.sh
+++ scripts/aws/aws-all-regions.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 _dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-for region in `aws ec2 describe-regions --output text | cut -f4`
-do
-echo -e "\nListing RDS in region:'$region'..."
+#for region in `aws ec2 describe-regions --output text | cut -f4`
+#do
+#echo -e "\nListing RDS in region:'$region'..."
+#aws rds describe-db-instances \
+	#--region $region | \
+  #yq '"\(.DBInstances[].EngineVersion) | \(.DBInstances[].DBInstanceIdentifier)"'
+#done
+
 aws rds describe-db-instances \
-	--region $region | \
-	yq '.DBInstances[].DBInstanceIdentifier'
-done
+	--region us-east-2 | \
+  yq '"\(.DBInstances[].DBInstanceIdentifier)"'
