b9e823be0a0b6968c538fa75e0561c45bdd7d37d
Erik Nelson <erik@nsk.io>
2024-01-04 11:37:51 -0500

    Add shellfn for monitoring disk writes

--- zshrc.d/shell_fn.sh
+++ zshrc.d/shell_fn.sh
@@ -30,3 +30,6 @@ pull_nexus() {
   done
 }
 
+watchdatwrite() {
+  watch -n1 'grep -E "(Dirty|Write)" /proc/meminfo; echo; ls /sys/block/ | while read device; do awk "{ print \"$device: \"  \$9 }" "/sys/block/$device/stat"; done'
+}
