c9348f663a02c5da0530b0c47beea3bdab94cd1d
Erik Nelson <erik@nsk.io>
2024-06-25 13:20:35 -0400

    Update hook with silver-server spec

--- zshrc.d/doscar_launch_hook.sh
+++ zshrc.d/doscar_launch_hook.sh
@@ -4,7 +4,7 @@ echo "========================================"
 echo "Configuring git+ssh to use ~./ssh/id.osc"
 export GIT_SSH_COMMAND="ssh -vvv -i $HOME/.ssh/id.osc"
 which tig || nix-env -i tig
-which ag || nix-env -i ag
+which ag || nix-env -iA nixpkgs.silver-searcher
 export PATH=$PATH:$HOME/nsk/bin
 ln -sf ~/.dotfiles/gitconfig ~/.gitconfig
 echo "========================================"
