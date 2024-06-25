a9a54a3a3678db126882e6584342fe9d1d4effe2
Erik Nelson <erik@nsk.io>
2024-06-25 11:09:32 -0400

    Update doscar hook to include the silver searcher

--- zshrc.d/doscar_launch_hook.sh
+++ zshrc.d/doscar_launch_hook.sh
@@ -4,6 +4,7 @@ echo "========================================"
 echo "Configuring git+ssh to use ~./ssh/id.osc"
 export GIT_SSH_COMMAND="ssh -vvv -i $HOME/.ssh/id.osc"
 which tig || nix-env -i tig
+which ag || nix-env -i ag
 export PATH=$PATH:$HOME/nsk/bin
 ln -sf ~/.dotfiles/gitconfig ~/.gitconfig
 echo "========================================"
