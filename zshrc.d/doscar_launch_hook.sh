997ee21e46dee29fe8fb93030e3e4ff01672b8a3
Erik Nelson <erik@nsk.io>
2024-06-26 08:57:16 -0400

    Continue to expand the doscar hook

--- zshrc.d/doscar_launch_hook.sh
+++ zshrc.d/doscar_launch_hook.sh
@@ -1,10 +1,41 @@
 echo "========================================"
 echo "NSK Doscar Hook"
 echo "========================================"
-echo "Configuring git+ssh to use ~./ssh/id.osc"
+
+gitconfigFile=~/.gitconfig
+nvimConfigDir=~/.config/nvim
+nvimInitFile=$nvimConfigDir/init.vim
+nvimAutoloadDir=~/.local/share/nvim/site/autoload
+plugFile=$nvimAutoloadDir/plug.vi
+
+################################################################################
+# Exports
+################################################################################
 export GIT_SSH_COMMAND="ssh -vvv -i $HOME/.ssh/id.osc"
-which tig || nix-env -i tig
-which ag || nix-env -iA nixpkgs.silver-searcher
 export PATH=$PATH:$HOME/nsk/bin
-ln -sf ~/.dotfiles/gitconfig ~/.gitconfig
+export GOROOT=/usr/local/go
+
+################################################################################
+# Alias
+################################################################################
+
+alias tg='tig HEAD~20..HEAD'
+alias vim='/usr/bin/vim'
+
+################################################################################
+# Custom installation
+################################################################################
+command -v tig || nix-env -iA nixpkgs.tig
+command -v ag || nix-env -iA nixpkgs.silver-searcher
+command -v nvim || nix-env -iA nixpkgs.neovim
+
+if [[ -e "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vi" ]]; then
+  mkdir -p $nvimAutoloadDir
+  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+fi
+
+[[ -e "$gitconfigFile" ]] || ln -sf ~/.dotfiles/gitconfig $gitconfigFile
+[[ -e "$nvimInitFile" ]] || ln -sf ~/.dotfiles/vimrc $nvimInitFile
+
 echo "========================================"
