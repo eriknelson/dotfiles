c9cef96e9b263690c445a14e020241282f4b5504
Erik Nelson <erik@nsk.io>
2024-06-27 16:26:26 -0400

    Update the hook to ensure nvim + plugins work

--- zshrc.d/doscar_launch_hook.sh
+++ zshrc.d/doscar_launch_hook.sh
@@ -5,7 +5,10 @@ echo "========================================"
 gitconfigFile=~/.gitconfig
 nvimConfigDir=~/.config/nvim
 nvimInitFile=$nvimConfigDir/init.vim
-nvimAutoloadDir=~/.local/share/nvim/site/autoload
+nvimAutoloadDir="${XDG_DATA_HOME:-$HOME/.local/share}"
+tmuxConfigFile=~/.tmux.conf
+tmuxConfigFabianFile=~/.tmux.f.conf
+tmuxConfigDarwinFile=~/.tmux.conf.darwin
 plugFile=$nvimAutoloadDir/plug.vi
 
 ################################################################################
@@ -20,7 +23,8 @@ export GOROOT=/usr/local/go
 ################################################################################
 
 alias tg='tig HEAD~20..HEAD'
-alias vim='/usr/bin/vim'
+alias vim='~/.nix-profile/bin/nvim'
+#alias vim='/u'
 
 ################################################################################
 # Custom installation
@@ -28,14 +32,23 @@ alias vim='/usr/bin/vim'
 command -v tig || nix-env -iA nixpkgs.tig
 command -v ag || nix-env -iA nixpkgs.silver-searcher
 command -v nvim || nix-env -iA nixpkgs.neovim
+unalias tmux
+command -v tmux || nix-env -iA nixpkgs.tmux
+alias tmux='tmux -2'
 
-if [[ -e "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vi" ]]; then
+if [[ ! -e "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vi" ]]; then
+  echo "Taking install branch"
   mkdir -p $nvimAutoloadDir
   sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+else
+  echo "Second branch"
 fi
 
 [[ -e "$gitconfigFile" ]] || ln -sf ~/.dotfiles/gitconfig $gitconfigFile
 [[ -e "$nvimInitFile" ]] || ln -sf ~/.dotfiles/vimrc $nvimInitFile
+[[ -e "$tmuxConfigFile" ]] || ln -sf ~/.dotfiles/tmux.conf ~/.tmux.conf
+[[ -e "$tmuxConfigFabianFile" ]] || ln -sf ~/.dotfiles/tmux.f.conf ~/.tmux.f.conf
+[[ -e "$tmuxConfigDarwinFile" ]] || ln -sf ~/.dotfiles/tmux.conf.darwin ~/.tmux.conf.darwin
 
 echo "========================================"
