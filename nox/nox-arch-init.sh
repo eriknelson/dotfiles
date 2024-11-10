c0dc3e9a43b4db776c91eb9c3ae45bf618a6fd1a
Erik Nelson <erik@nsk.io>
2024-11-10 13:58:05 -0500

    Moving some things around

--- nox/nox-arch-init.sh
+++ nox/nox-arch-init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 _dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-essentialsFile=$_dir/../nox-arch-pkg-essentials.txt
+essentialsFile=$_dir/nox-arch-pkg-essentials.txt
 flatStr=$(cat $essentialsFile | tr '\n' ' ')
 echo $flatStr
 sudo pacman -S $flatStr
