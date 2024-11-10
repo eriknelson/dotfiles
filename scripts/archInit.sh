f92fd25efc0de48d897a9769a05596d4f03c5b3b
Erik Nelson <erik@nsk.io>
2024-11-10 13:18:34 -0500

    Update archInit script to include module loading

--- scripts/archInit.sh
+++ scripts/archInit.sh
@@ -4,3 +4,12 @@ essentialsFile=$_dir/../nox-arch-pkg-essentials.txt
 flatStr=$(cat $essentialsFile | tr '\n' ' ')
 echo $flatStr
 sudo pacman -S $flatStr
+
+################################################################################
+# Modules
+################################################################################
+# i2c-dev is for detecting the display identifiers in a reliable manner based
+# on an "edid" that is embedded within the monitors. It's included in the
+# arch linux package, so no extra package install is needed, but it still
+# needs to be manually loaded.
+echo 'i2c-dev' | sudo tee /etc/modules-load.d/i2c-dev.conf
