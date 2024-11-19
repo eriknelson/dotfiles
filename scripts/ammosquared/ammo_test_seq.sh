022f8f59ab9c3de2d0bb7f71af7de89d2540bab1
Erik Nelson <erik@nsk.io>
2024-11-19 15:14:57 -0500

    Add ammo testing scripts

--- scripts/ammosquared/ammo_test_seq.sh
+++ scripts/ammosquared/ammo_test_seq.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 _dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd ~/git/ammosquared/web-platform
+cd $AMMOSQ_MAIN
 
 timestamp=$(date +"%H%M%S")
 testfile="/tmp/ammosq-test-$timestamp.txt"
 echo "RSpec output file: $testfile"
-
-DBUSER=postgres \
-DBHOST="db.lb.legion.kotawerks.gg" \
-DBPASSWORD="$ADMIN_DB_PASSWD" \
-DISABLE_SPRING=true \
-RAILS_ENV=test \
 
 time bundle exec rspec \
   --exclude-pattern "spec/system/**/*" \
